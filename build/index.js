6e7498a795377ddafec1f8dcd93866f2c9c9678d
rgiro <rociogiro@yahoo.com>
2023-12-14 22:02:10 -0500

    install jasmine

---
+++ build/index.js
@@ -0,0 +1,4 @@
+"use strict";
+const myName = "your name";
+const hello = (userName) => `hello, ${userName}`;
+console.log(hello(myName));
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const myName = 'your name';
+
+const hello = (userName: string): string => `hello, ${userName}`;
+
+console.log(hello(myName));
+console.log('hello again');
